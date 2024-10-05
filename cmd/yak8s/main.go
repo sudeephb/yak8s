54835b74b3a0170f01992c6b214b4c92b79c0ef7
sudeephb <28506885+sudeephb@users.noreply.github.com>
2024-10-05 23:29:53 +0545

    Add yak8s up command

--- cmd/yak8s/main.go
+++ cmd/yak8s/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"yak8s/pkg/cli"
 )
 
 func main() {
-	fmt.Println("Starting yak8s...")
-
-	// Call CLI command handler (provision in this case)
-	if err := cli.RunProvisionCommand(3); err != nil {
-		fmt.Printf("Error provisioning VMs: %v\n", err)
-		return
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: yak8s <command>")
+		fmt.Println("Exiting...")
+		os.Exit(1)
 	}
 
-	fmt.Println("Successfully provisioned 3 VMs")
+	// This is currently just 'up' command
+	command := os.Args[1]
+	switch command {
+	case "up":
+		fmt.Println("Running yak8s up...")
+		fmt.Println("Spinning up VMs to deploy the cluster.")
+		numVms := 3
+		if err := cli.RunProvisionCommand(numVms); err != nil {
+			fmt.Printf("Error provisioning VMs: %v\n", err)
+			return
+		}
+		fmt.Printf("Successfully provisioned %d VMs\n", numVms)
+	default:
+		fmt.Printf("Unknown command: %s\n", command)
+		os.Exit(1)
+	}
+
 }
